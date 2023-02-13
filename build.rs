fae458cce4380cbabe952e9ac178a221b61a6557
Florian Beck <florian.f.beck@web.de>
2023-02-13 22:57:38 +0100

    Updated comments

--- build.rs
+++ build.rs
@@ -7,8 +7,7 @@ use std::{
 };
 
 fn main() {
-   // Put `memory.x` in our output directory and ensure it's
-   // on the linker search path.
+   // Put memory layout in the output directory and ensure it's on the linker search path.
    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
    File::create(out.join("memory.x"))
        .unwrap()
@@ -16,10 +15,7 @@ fn main() {
        .unwrap();
    println!("cargo:rustc-link-search={}", out.display());
 
-   // By default, Cargo will re-run a build script whenever
-   // any file in the project changes. By specifying `memory.x`
-   // here, we ensure the build script is only re-run when
-   // `memory.x` is changed.
+   // By default, Cargo will re-run a build script whenever any file in the project changes.
+   // Specify `memory.x` here to ensure it is only re-run when `memory.x` is changed.
    println!("cargo:rerun-if-changed=memory.x");
 }
- 
